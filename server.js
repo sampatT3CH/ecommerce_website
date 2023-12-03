bdbeb67d4ddc622d2ca3ea5c7306ed45b4fb958f
sampatT3CH <138952828+sampatT3CH@users.noreply.github.com>
2023-12-03 18:11:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import cors from 'cors';
-import path from "path"
+import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -15,14 +16,17 @@ const app = express();
 
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.use('/api/v1/auth', authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname, './clirnt/build')))
 app.use("*", function  (req,res) {
     res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
